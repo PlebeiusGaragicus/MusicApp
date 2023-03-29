61b88d5bd030cd5a17537c7f685367e632468e82
plebg <plebeiusgaragicus@gmail.com>
2023-03-28 17:55:42 -0700

    fix

--- public/config.js
+++ public/config.js
@@ -1,9 +1,12 @@
+// 143.42.165.77
+// veggietunes.com
+
 const config = {
     WEBSOCKET_URL: 'ws://localhost:3000', // Default to localhost for development
 };
 
 if (window.location.hostname !== 'localhost') {
-    config.WEBSOCKET_URL = 'ws://veggietunes.com:3000'; // Replace with your server's IP or domain
+    config.WEBSOCKET_URL = 'ws://143.42.165.77:3000'; // Replace with your server's IP or domain
 }
 
 export default config;
